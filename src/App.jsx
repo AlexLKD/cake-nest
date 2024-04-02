4761b29848a2f9f176b1f5572d901579ed931f51
Alex <adubrez1409@gmail.com>
2024-04-02 12:39:07 +0200

    form style

--- src/App.jsx
+++ src/App.jsx
@@ -4,42 +4,43 @@ import './App.css'
 function App() {
 
   // state (données)
+
   const [firstName, setFirstName] = useState('')
   const [errorMessage, setErrorMessage] = useState('')
 
   // action comportements
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (firstName.trim() === '') {
-      setErrorMessage('Le champ prénom est obligatoire.');
-    } else {
-      alert(`Bonjour ${firstName}`);
-      setFirstName('');
-      setErrorMessage('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const message = firstName.trim() === '' ? 'Le champ prénom est obligatoire.' : `Bonjour ${firstName}`;
+    setErrorMessage(message === `Bonjour ${firstName}` ? '' : message);
+    if (message === `Bonjour ${firstName}`) {
+      setFirstName(''); 
+      alert(message);
     }
-  }
+  };
+  
 
-  const handleChange = (event) => {
-    setFirstName(event.target.value);
+  const handleChange = (e) => {
+    setFirstName(e.target.value);
   };
 
-  //affichage
+  // affichage
 
   return (
     <>
         <h1>Bienvenue chez nous !</h1>
         <h2>Connectez-vous </h2>
-        {errorMessage && <div className="error">{errorMessage}</div>}
         <form onSubmit={handleSubmit}>
           <input
             type="text"
             value={firstName}
             onChange={handleChange}
             placeholder="Entrez votre prénom..."
-          />
+            />
         <button type="submit">Accéder à votre espace</button>
       </form>
+      {errorMessage && <div className="error">{errorMessage}</div>}
     </>
   )
 }
