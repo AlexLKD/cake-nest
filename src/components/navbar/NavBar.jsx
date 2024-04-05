2d25a45464e7b069dc62bfa0755e3463c5f5005b
Alex <adubrez1409@gmail.com>
2024-04-05 14:44:51 +0200

    F06

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -2,19 +2,68 @@ import React from 'react';
 import styled from 'styled-components';
 import LeftSide from './LeftSide';
 import RightSide from './RightSide';
-
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useState } from 'react';
 
 const Navbar = ({ username }) => {
+
+  const notifyAdminActive = () => {
+    toast.success('Mode admin activé', {
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+      });
+  }
+
+  const notifyAdminNotActive = () => {
+    toast.error('Mode admin désactivé', {
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+  };
+
+  const [isAdminMode, setIsAdminMode] = useState(false);
+
+  const toggleAdminMode = () => {
+    setIsAdminMode(!isAdminMode);
+
+    if (!isAdminMode) {
+      notifyAdminActive();
+    } else {
+      notifyAdminNotActive();
+    }
+  };
+
   return (
     <NavBarStyled>
       <LeftSide />
+      <div className="navbar">
+        {isAdminMode ? (
+          <button className='admin-btn' id="adminButton" onClick={toggleAdminMode}>DÉSACTIVER LE MODE ADMIN</button>
+        ) : (
+          <button className='admin-btn' id="adminButton" onClick={toggleAdminMode}>ACTIVER LE MODE ADMIN</button>
+        )}
+        <ToastContainer />
+      </div>
       <RightSide username={username} />
     </NavBarStyled>
   );
 };
 
 export default Navbar;
+
 
 const NavBarStyled = styled.div`
   width: 100%;
@@ -28,4 +77,14 @@ const NavBarStyled = styled.div`
   justify-content: space-between;
   align-items: center;
   margin-top: 20px;
+
+  .admin-btn {
+    padding: 10px 20px;
+  background-color: teal;
+  color: white;
+  border: none;
+  cursor: pointer;
+  }
+
+  
 `;
